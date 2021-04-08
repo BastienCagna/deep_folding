b8d047dfeac833407e960fd168b11e6792e40e4d
Louise GUILLON <lg261972@is234205.intra.cea.fr>
2021-04-08 14:51:53 +0200

    function merge simple surfacein progress

--- deep_folding/anatomist_tools/benchmark_generation.py
+++ deep_folding/anatomist_tools/benchmark_generation.py
@@ -1,10 +1,50 @@
-# The aim of this script is to generate a benchmark of sulcal abnormalities.
-# In this script, abnormalities are defined as skeleton with one simple surface
-# missing. This simple surface must be completely in the bounding box of interest
-#(currently S.T.s.ter.asc.ant/post Left) and include a minimum number of voxels
-# (in order that the anomaly is big enough to be considered as abnormal).
+# -*- coding: utf-8 -*-
+# /usr/bin/env python2.7 + brainvisa compliant env
+#
+#  This software and supporting documentation are distributed by
+#      Institut Federatif de Recherche 49
+#      CEA/NeuroSpin, Batiment 145,
+#      91191 Gif-sur-Yvette cedex
+#      France
+#
+# This software is governed by the CeCILL license version 2 under
+# French law and abiding by the rules of distribution of free software.
+# You can  use, modify and/or redistribute the software under the
+# terms of the CeCILL license version 2 as circulated by CEA, CNRS
+# and INRIA at the following URL "http://www.cecill.info".
+#
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using,  modifying and/or developing or reproducing the
+# software by the user in light of its specific status of free software,
+# that may mean  that it is complicated to manipulate,  and  that  also
+# therefore means  that it is reserved for developers  and  experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and,  more generally, to use and operate it in the
+# same conditions as regards security.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license version 2 and that you accept its terms.
 
-# Modules import
+"""Generating benchmarks of sulcal abnormalities.
+Abnormalities are defined as skeletons with one simple surface missing or one
+additional simple surface. This simple surface must be completely in the
+bounding box of interest (currently S.T.s.ter.asc.ant/post Left) and include
+a minimum number of voxels (in order that the anomaly is big enough to be
+considered as abnormal).
+"""
+
+######################################################################
+# Imports and global variables definitions
+######################################################################
+
 from soma import aims
 import numpy as np
 from glob import glob
@@ -12,7 +52,17 @@ import random
 import pandas as pd
 import os
 
-def generate(b_num, side, region, ss_size):
+
+def get_sub_list():
+
+    right_handed = pd.read_csv('/neurospin/dico/lguillon/hcp_info/right_handed.csv')
+    subjects_list = list(right_handed['Subject'])
+    random.shuffle(subjects_list)
+
+    return subjects_list
+
+
+def generate_missing_ss(b_num, side, region, ss_size):
     """
     IN: b_num: benchmark number
         side: hemisphere, str, whether 'L' or 'R',
@@ -22,14 +72,14 @@ def generate(b_num, side, region, ss_size):
          list of subjects altered or original
     """
     # List of right handed subjects
-    right_handed = pd.read_csv('/neurospin/dico/lguillon/hcp_info/right_handed.csv')
-    subjects_list = list(right_handed['Subject'])
+    # right_handed = pd.read_csv('/neurospin/dico/lguillon/hcp_info/right_handed.csv')
+    # subjects_list = list(right_handed['Subject'])
 
     data_dir = '/neurospin/hcp/ANALYSIS/3T_morphologist/' # folder containing all
                                                           # HCP subjects folder
     saving_dir = '/neurospin/dico/lguillon/mic21/anomalies_set/dataset/benchmark' + str(b_num) + '/0_' + side + 'side/'
-    #folder_list = glob(data_dir + '*') # get list of all subjects folder
-    random.shuffle(subjects_list)
+
+    subjects_list = get_sub_list()
 
     # Saving of simple surfaces satisfying both criteria: completely inside the
     # bounding box defined for the crop and including at least 500 voxels
@@ -58,7 +108,6 @@ def generate(b_num, side, region, ss_size):
                             if bucket.size() > ss_size: # In order to keep only large enough simple surfaces
                                 surfaces[len(surfaces)] = v
 
-            print(len(surfaces.keys()))
             if len(surfaces.keys()) > 0:
                 # Suppression of one random simple surface (satisfying both criteria)
                 surface = random.randint(0, len(surfaces)-1)
@@ -97,9 +146,77 @@ def generate(b_num, side, region, ss_size):
     df_abnor_test.to_csv(saving_dir + 'abnormality_test.csv')
 
 
+def generate_add_ss(b_num, side, region, ss_size):
+    """
+    Creates a benchmark with skeletons having additional simple surfaces (ss)
+    IN: b_num: benchmark number
+        side: hemisphere, str, whether 'L' or 'R',
+        region: region of the brain
+        ss_size: Minimal size of simple surface to suppress
+    OUT: altered skeletons generated
+         list of subjects altered or original
+    """
+    n = 10 # benchmark size
+    data_dir = '/neurospin/hcp/ANALYSIS/3T_morphologist/' # folder containing all                                                          # HCP subjects folder
+    saving_dir = '/neurospin/dico/lguillon/mic21/anomalies_set/dataset/benchmark' + str(b_num) + '/0_' + side + 'side/'
+
+    subjects_list = get_sub_list()
+    #dict_sub = {k: [] for k in subjects_list}
+    surfaces = dict()
+    dict_sub = dict()
+
+    # Saving of simple surfaces satisfying both criteria: completely inside the
+    # bounding box defined for the crop and including at least 500 voxels
+
+    abnormality_test = []
+    if region == 'S.T.s':
+        bbmin = [34.05417004,  39.15836927, -65.53190718] # Obtained with bbox_definition.py script
+        bbmax = [71.73703268, 85.71765662, -2.74244928] # Obtained with bbox_definition.py script
+
+    for sub in subjects_list[:20]:
+        print(sub)
+        if os.path.isdir(data_dir + str(sub)):
+            graph = aims.read(data_dir + str(sub) + '/t1mri/default_acquisition/default_analysis/folds/3.1/default_session_auto/'+ side + str(sub) + '_default_session_auto.arg')
+
+            for v in graph.vertices():
+                if 'label' in v:
+                    bbmin_surface = v['Tal_boundingbox_min']
+                    bbmax_surface = v['Tal_boundingbox_max']
+                    bck_map = v['aims_ss']
+
+                    if all([a >= b for (a, b) in zip(bbmin_surface, bbmin)]) and all([a <= b for (a, b) in zip(bbmax_surface, bbmax)]):
+                        for bucket in bck_map:
+                            if bucket.size() > ss_size: # In order to keep only large enough simple surfaces
+                                surfaces[len(surfaces)] = v
+                                if sub in dict_sub.keys():
+                                    dict_sub[sub].append(len(surfaces))
+                                else:
+                                    dict_sub[sub] = [len(surfaces)]
+    print(dict_sub)
+    for k in range(3):
+        subject = dict_sub.keys()[k]
+        surface = random.randint(0, len(surfaces)-1) # surface to add
+
+        # skeleton where to add
+        skel = aims.read(data_dir + str(subject) + '/t1mri/default_acquisition/default_analysis/segmentation/' + side + 'skeleton_' + str(subject) + '.nii.gz')
+
+        bck_map = surfaces[surface]['aims_ss']
+        for voxel in bck_map[0].keys():
+            skel.setValue(1, voxel[0], voxel[1], voxel[2])
+
+        bck_map_bottom = surfaces[surface]['aims_bottom']
+        for voxel in bck_map_bottom[0].keys():
+            skel.setValue(1, voxel[0], voxel[1], voxel[2])
+
+        # Writting of output graph
+        fileout = saving_dir + 'output_skeleton_' + str(sub) + '.nii.gz'
+        print('writing altered skeleton to', fileout)
+        aims.write(skel, fileout)
+
+
 if __name__ == '__main__':
-    benchmark_num = 2
+    benchmark_num = 3
     side = 'L'
     region = 'S.T.s'
-    ss_size = 600
-    generate(benchmark_num, side, region, ss_size)
+    ss_size = 200
+    generate_add_ss(benchmark_num, side, region, ss_size)
